46a4f8fdf6de89ce6db711cc71c4689cf56e0acf
Branko Alvarez <bm.developer@outlook.es>
2022-11-08 22:42:52 -0300

    Agregar el Navbar por Bootstap

--- .next/static/webpack/webpack.c7f40ea4f4a9375d.hot-update.js
+++ .next/static/webpack/webpack.c7f40ea4f4a9375d.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "1808d0cc568422d3"; }
+/******/ 	__webpack_require__.h = function() { return "9791e77938f5c6e4"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,43 @@
-const Navbar = () => <h1>Navbar</h1>
+const Navbar = () => (
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        
+        <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+            <ul class="navbar-nav mr-auto">
+
+                <li class="nav-item active">
+                    <a class="nav-link active" href="#">Acerca de Nosotros</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Medicos</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Contactanos</a>
+                </li>
+
+            </ul>
+        </div>
+
+        <div class="mx-auto order-0">
+            <a class="navbar-brand mx-auto" href="#">CLINICA O LOGO</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+
+        <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <button type="button" class="btn btn-light">REGISTRARSE</button>
+                </li>
+                <li class="nav-item">
+                    <button type="button" class="btn btn-light">INICIAR SESION</button>
+                </li>
+            </ul>
+        </div>
+    </nav>
+)
 
 
 export default Navbar;
