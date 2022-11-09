e3fe7df10723441f97bc6d56a403cc924040eafd
Daravena22 <daravena1997@gmail.com>
2022-11-09 11:54:47 -0300

    Mi primer cambio en el readme

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b16e8ab8abad3776"; }
+/******/ 		__webpack_require__.h = function() { return "d7be4557e3c76384"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
